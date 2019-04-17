dc4549780a2c157bfcd0047eccb7b233368b4284
Ella Mathewson (RIT Student) <exm1381@ad.rit.edu>
2019-04-17 14:56:14 -0400

    Fixed it almost!! Added code using Dark Sky API to access it

---
+++ javascript.js
@@ -0,0 +1,6 @@
+/*let request = new XMLHttpRequest();
+request.open('GET', 'https://api.twitch.tv/kraken/search/games?query=star', true);
+request.onload = function () {
+    console.log("test");
+}
+request.send();*/
