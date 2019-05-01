82e9d6b5e6b8c47fa2dbe139dcf22427288fd53e
Joshua Bullock (RIT Student) <jtb3607@ad.rit.edu>
2019-04-30 20:26:05 -0400

    got string check to work, limited responses on admin page, added es6

--- javascript.js
+++ javascript.js
@@ -3,10 +3,15 @@
 
 //check for strings
 
-//find out what mvvm framework is
+
 
 //validate html and css
-
+class Position{
+    constructor(lat,long){
+        this.lat= lat;
+        this.long = long;
+    }
+}
 
 const app = new Vue({
             el: '#app',
@@ -14,18 +19,18 @@ const app = new Vue({
                 title: "Project 4",
                 result: "loading",
                 geoResult: "loading",
-                currentLat: '43.1566',
-                currentLong: '77.6088',
                 Askey: "a54c0fb6b7947c6785acbbd4327d5dd437f87cc9",
                 AsID: "dfedcd9c-19ac-4511-8be7-94fcbae1db09",
                 AsDomain: "https://people.rit.edu",
                 token: "",
-                database: ""
+                database: "",
+                inputError: false,
+                position: new Position(43.1566,77.6088) 
             },
             created() {
                 //this.search()
                 // Initialize Firebase
-                var config = {
+                let config = {
                     apiKey: "AIzaSyAibUl8D6kTXGhxO8xcCocygjRbyo712Wk",
                     authDomain: "starfinder-7daeb.firebaseapp.com",
                     databaseURL: "https://starfinder-7daeb.firebaseio.com",
@@ -59,7 +64,10 @@ const app = new Vue({
                     //console.log(limit);
                     //if (! this.term.trim()) return;
                     //https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/find?q=London&appid=5e16c543219fc585e74c6275f9450fa5
-                    let url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/605bcd829d90fb8c0a272b66244388e7/" + this.currentLat + "," + this.currentLong;
+                    if(this.inputError){
+                        return
+                    }
+                    let url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/605bcd829d90fb8c0a272b66244388e7/" + this.position.lat + "," + this.position.long;
                     console.log(url);
                     fetch(url)
                         .then(response => {
@@ -116,8 +124,11 @@ const app = new Vue({
                         })
                 },
                 geoLocation() {
+                    if(this.inputError){
+                        return
+                    }
                     let ref = this.database.ref('geoLocations');
-                    let url = "https://api.ipgeolocation.io/astronomy?apiKey=99a7a2ef78ff4e2193dda6c3bd1af8c5&lat=" + this.currentLat + "&long=" + this.currentLong;
+                    let url = "https://api.ipgeolocation.io/astronomy?apiKey=99a7a2ef78ff4e2193dda6c3bd1af8c5&lat=" + this.position.lat + "&long=" + this.position.long;
                     console.log(url);
                     fetch(url)
                         .then(response => {
@@ -137,17 +148,32 @@ const app = new Vue({
                             ref.push(this.geoResult);
                             //console.log(this.result.currently.temperature);
                         })
+                },
+                check(){
+                    
+                    if(isNaN(this.position.lat) || isNaN(this.position.long)){
+                        this.inputError = true;
+                    }else{
+                        this.inputError = false;
+                    }
+                    
+                    if(this.position.lat == "" || this.position.long == ""){
+                        this.inputError = true;
+                    }else{
+                        this.inputError = false;
+                    }
+
                 } //end of search
             } // end methods
         });
 
 
-$("#load").hide();
+// $("#load").hide();
 
-$(".btn").click(function() {
-    $("#load").show();
-    $("#allResults").ready(function() {
-        $("#allResults").show();
-        $("#load").hide();
-    });
-})
+// $(".btn").click(function() {
+//     $("#load").show();
+//     $("#allResults").ready(function() {
+//         $("#allResults").show();
+//         $("#load").hide();
+//     });
+// })
